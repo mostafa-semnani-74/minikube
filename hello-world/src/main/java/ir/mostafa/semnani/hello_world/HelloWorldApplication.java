2d57fc4f5ba92d677e1afc4b02feaf45f8e76151
Mostafa Semnani <mostafa.semnani.74@gmail.com>
2024-09-04 10:17:24 +0330

    init hello-world deployment on kubernetes

---
+++ hello-world/src/main/java/ir/mostafa/semnani/hello_world/HelloWorldApplication.java
@@ -0,0 +1,22 @@
+package ir.mostafa.semnani.hello_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello() {
+		return "Hello World";
+	}
+
+}
